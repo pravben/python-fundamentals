e0fa9ef355f651f3325bf8f5669b5975cc83394b
PRAVEEN BENNY <115497443+pravben@users.noreply.github.com>
2022-11-01 16:37:04 +0530

    Create oddeven.py

---
+++ day 1/oddeven.py
@@ -0,0 +1,10 @@
+x=int(input("Enter a number"))
+
+if x%2==0 :
+    if x>2:
+        print("The number is greater than 2 and is even")
+    
+    else:
+         print("The number is greater than is even")
+else:
+    print("The number is odd")
